729126b4f17d665f9d6caf539215b4896c563c25
masterj <ronchaos@gmail.com>
2023-09-11 23:04:21 +0900

    Initial Commit

---
+++ instalgram-st/instalgram_stApp.swift
@@ -0,0 +1,17 @@
+//
+//  instalgram_stApp.swift
+//  instalgram-st
+//
+//  Created by masterj on 2023/09/11.
+//
+
+import SwiftUI
+
+@main
+struct instalgram_stApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
